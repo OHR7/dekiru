80a7b8bb5065d8ffc48341037ad40917963f3594
OHR7 <oherrera@uabc.edu.mx>
2018-07-29 13:55:35 +0900

    - Added pages app to handle common views across the main app - Added the profile app to handle the user logic - Added home, login and base html files - Added Django Auth urls

---
+++ dekiru/settings/local.py
@@ -0,0 +1,25 @@
+from .base import *
+
+INSTALLED_APPS += [
+	'pages',
+	'profiles',
+]
+
+LOGIN_REDIRECT_URL = '/'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, '../templates')]
+		,
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
--- dekiru/urls.py
+++ dekiru/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+	path('admin/', admin.site.urls),
+	path('', include('django.contrib.auth.urls')),
+
+	# Pages URL's
+	path('', include('pages.urls')),
+
+	# Profile URL's
+	path('profiles/', include('profiles.urls')),
+
 ]
